9295828b8c971c9efd9b366926d60af201834d04
Geovani Orsoli <112351284+geovaniorsoli@users.noreply.github.com>
2023-12-05 12:07:26 -0300

    comit

--- public/js/event.js
+++ public/js/event.js
@@ -1,15 +1,21 @@
 function dinamic(){
     const select = document.getElementById('dinamic').value
+    const selectMargin = document.getElementById('dinamic')
 
     const form = document.getElementById('container-form')
     const list = document.getElementById('container-list')
+    const title = document.getElementById('container-title')
+    
 
     if(select === 'inserir'){
         form.style.display = 'block'
         list.style.display = 'none'
+        title.style.display = 'block'
     } else if (select === 'visualizar'){
         form.style.display = 'none'
         list.style.display = 'block'
+        title.style.display = 'none'
+        selectMargin.className = 'select mt-4'
     }
 
 }
